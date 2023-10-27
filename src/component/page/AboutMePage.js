65695a4d23feb0fe35c5fb372a611930f268599a
RoganYeo <yic6060@gmail.com>
2023-10-27 16:23:13 +0900

    AboutMe 화면 랜딩 페이지 누락으로 재커밋

--- src/component/page/AboutMePage.js
+++ src/component/page/AboutMePage.js
@@ -112,31 +112,31 @@ function AboutMePage(props) {
 
       <div className="h-full bg-slate-100"></div>
 
-      {/*<div*/}
-      {/*  className={classNames(*/}
-      {/*    "absolute inset-0 z-40 h-full w-full bg-white dark:bg-black ",*/}
-      {/*    isHidden ? "animate-fadeOut" : "",*/}
-      {/*  )}*/}
-      {/*>*/}
-      {/*  <div className="flex h-full w-full flex-col items-center justify-center gap-x-1 sm:flex-row ">*/}
-      {/*    <p className="w-[20rem] text-right text-[13rem] font-bold dark:text-amber-500 sm:text-right">*/}
-      {/*      '{age}*/}
-      {/*    </p>*/}
-      {/*    <div>*/}
-      {/*      <p className="font-NanumMyeongjo text-2xl font-bold dark:text-white sm:text-3xl sm:font-bold md:text-5xl md:font-normal">*/}
-      {/*        이렇게 살 수도*/}
-      {/*        <br /> 이렇게 죽을 수도 없을 때<br /> 서른 살은 온다*/}
-      {/*        <p*/}
-      {/*          className={classNames(*/}
-      {/*            "text-left font-NanumMyeongjo text-sm text-gray-400",*/}
-      {/*          )}*/}
-      {/*        >*/}
-      {/*          최승자의 시 "삼십세" 중에서*/}
-      {/*        </p>*/}
-      {/*      </p>*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
+      <div
+        className={classNames(
+          "absolute inset-0 z-40 h-full w-full bg-white dark:bg-black ",
+          isHidden ? "animate-fadeOut" : "",
+        )}
+      >
+        <div className="flex h-full w-full flex-col items-center justify-center gap-x-1 sm:flex-row ">
+          <p className="w-[20rem] text-right text-[13rem] font-bold dark:text-amber-500 sm:text-right">
+            '{age}
+          </p>
+          <div>
+            <p className="font-NanumMyeongjo text-2xl font-bold dark:text-white sm:text-3xl sm:font-bold md:text-5xl md:font-normal">
+              이렇게 살 수도
+              <br /> 이렇게 죽을 수도 없을 때<br /> 서른 살은 온다
+              <p
+                className={classNames(
+                  "text-left font-NanumMyeongjo text-sm text-gray-400",
+                )}
+              >
+                최승자의 시 "삼십세" 중에서
+              </p>
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
